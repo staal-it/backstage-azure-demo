459f255d397d42e657104a5a02d34b06753e3da3
Erwin Staal <erwin.staal@xebia.com>
2026-01-31 14:25:19 +0100

    Initial setup

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "storage_account_name" {
+  description = "The name of the storage account"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region for the storage account"
+  type        = string
+  default     = "westeurope"
+}
